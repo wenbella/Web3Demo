bb61970bf011353efd88ea007b98e63b318a4df1
wenbella <yingwenren2014@gmail.com>
2022-11-15 10:09:49 +0800

    image object

--- img2img_stableDifussion.py
+++ img2img_stableDifussion.py
@@ -28,11 +28,11 @@ def img2img(imgobject, prompt):
     init_image = init_image.resize((768, 512))
     with autocast("cuda"):
         images = pipe(prompt=prompt, init_image=init_image, strength=0.75, guidance_scale=7.5).images
-    return images
+    return images[0]
 
 
 if __name__ == "__main__":
     imgobject = Image.open("test.jpg")
     images = img2img(imgobject, "A very beautiful anime girl, full body, long wavy blond hair, sky blue eyes")
-    images[0].show()
-    images[0].save("pretty.png")
+    images.show()
+    images.save("pretty.png")
