2f236c5ab9604d3be303ee4352e9a48e7df9104c
wenbella <yingwenren2014@gmail.com>
2022-11-15 11:53:45 +0800

    img2img

--- img2img_stableDifussion.py
+++ img2img_stableDifussion.py
@@ -5,10 +5,12 @@
 # pip install --ignore-installed Pillow==9.0.0
 # pip install accelerate
 
+import os
 from torch import autocast
 import torch
 import requests
 from PIL import Image
+import uuid
 from io import BytesIO
 
 from diffusers import StableDiffusionImg2ImgPipeline
@@ -23,16 +25,24 @@ pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
 ).to(device)
 
 
-def img2img(imgobject, prompt):
+def img2img(img_path, prompt):
+    imgobject = Image.open(img_path)
+    img_path_lst = []
+    abs_path = os.path.dirname(os.path.realpath(__file__)) + "/images/"
+    if not os.path.exists(abs_path):
+        os.mkdir(abs_path)
     init_image = imgobject.convert("RGB")
     init_image = init_image.resize((768, 512))
     with autocast("cuda"):
         images = pipe(prompt=prompt, init_image=init_image, strength=0.75, guidance_scale=7.5).images
-    return images[0]
+        file_name = str(uuid.uuid4())
+        img_path = abs_path + f"{file_name}.jpg"
+        images.save(img_path)
+        img_path_lst.append(img_path)
+    return img_path
 
 
 if __name__ == "__main__":
-    imgobject = Image.open("test.jpg")
-    images = img2img(imgobject, "A very beautiful anime girl, full body, long wavy blond hair, sky blue eyes")
-    images.show()
-    images.save("pretty.png")
+    # imgobject = Image.open("test.jpg")
+    img_path = img2img("test.jpg", "A very beautiful anime girl, full body, long wavy blond hair, sky blue eyes")
+    print(img_path)
